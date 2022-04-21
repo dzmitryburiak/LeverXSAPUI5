d92bd1e35c15750e3a2c20bf517d884a05f9f4ed
Dzmitry Buriak <dzmitry.buriak@sap.com>
2022-04-21 12:29:48 +0000

    Add list of users

--- demoapp/webapp/controller/App.controller.js
+++ demoapp/webapp/controller/App.controller.js
@@ -1,12 +1,37 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, MessageBox, JSONModel) {
     "use strict";
     return Controller.extend("lx.sapui5.demoapp.controller.App", {
+        onInit : function () {
+            var aUsers = [
+                {name: "Dzmitry", age: 34},
+                {name: "Vasili", age: 23},
+                {name: "Aliaksei", age: 30}
+            ]
+            var oModel = new JSONModel({users: aUsers});
+
+            this.getView().setModel(oModel);
+        },
         onButtonPress: function () {
-            var name = this.getView().byId("nameInput").getValue() || "Stranger";
-            MessageToast.show(`Hello ${name}!`);
+            var oModel = this.getView().getModel();
+            var sName = oModel.getProperty("/name");
+            var sAge = oModel.getProperty("/age");
+
+            if (sName && sAge) {
+                var aUsers = oModel.getProperty("/users");
+
+                aUsers.push({name: sName, age: sAge});
+                oModel.setProperty("/users", aUsers);
+                MessageToast.show(`New user ${sName} ${sAge} y.o. has been created`);
+            } else {
+                MessageBox.error(
+                    "Oops there was a problem, please check your input!"
+                );
+            }            
         }
     });
 });
