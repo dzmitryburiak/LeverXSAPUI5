a419df0103781babad6ebe8cdb21012261cb9c4e
Dzmitry Buriak <dzmitry.buriak@sap.com>
2022-04-21 14:14:47 +0000

    Add fragment. Refactor UI

--- demoapp/webapp/controller/App.controller.js
+++ demoapp/webapp/controller/App.controller.js
@@ -2,8 +2,9 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/m/MessageBox",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, MessageBox, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment"
+], function (Controller, MessageToast, MessageBox, JSONModel, Fragment) {
     "use strict";
     return Controller.extend("lx.sapui5.demoapp.controller.App", {
         onInit : function () {
@@ -11,12 +12,29 @@ sap.ui.define([
                 {name: "Dzmitry", age: 34},
                 {name: "Vasili", age: 23},
                 {name: "Aliaksei", age: 30}
-            ]
+            ];
             var oModel = new JSONModel({users: aUsers});
 
             this.getView().setModel(oModel);
         },
-        onButtonPress: function () {
+        onAddUserPress: function () {
+            if (this._oDialog) {
+                this._oDialog.open();
+            } else {
+                Fragment.load({
+                    name: "lx.sapui5.demoapp.fragment.NewUserDialog",
+                    controller: this
+                }).then(function(oDialog){
+                   this._oDialog = oDialog;
+                   this.getView().addDependent(oDialog);
+                   oDialog.open();
+                }.bind(this));
+            }           
+        },
+        onCancelPress: function () {
+            this._oDialog.close();            
+        },
+        onCreateUserPress: function () {
             var oModel = this.getView().getModel();
             var sName = oModel.getProperty("/name");
             var sAge = oModel.getProperty("/age");
@@ -27,11 +45,17 @@ sap.ui.define([
                 aUsers.push({name: sName, age: sAge});
                 oModel.setProperty("/users", aUsers);
                 MessageToast.show(`New user ${sName} ${sAge} y.o. has been created`);
+                this._oDialog.close();
             } else {
                 MessageBox.error(
                     "Oops there was a problem, please check your input!"
                 );
             }            
+        },
+        onAfterCloseDialog: function () {
+            var oModel = this.getView().getModel();
+            oModel.setProperty("/name", "");
+            oModel.setProperty("/age", "");
         }
     });
 });
