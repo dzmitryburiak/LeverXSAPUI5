5dfe60f1524e81e8283482c418259c792ead545c
Dzmitry Buriak <dzmitry.buriak@sap.com>
2022-04-21 12:01:15 +0000

    Change alert to MessageToast

--- demoapp/webapp/controller/App.controller.js
+++ demoapp/webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("lx.sapui5.demoapp.controller.App", {
         onButtonPress: function () {
             var name = this.getView().byId("nameInput").getValue() || "Stranger";
-            alert(`Hello ${name}!`);
+            MessageToast.show(`Hello ${name}!`);
         }
     });
 });
